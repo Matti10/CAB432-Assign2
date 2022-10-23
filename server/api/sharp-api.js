841e6ca5777d52b2d0bbdc18e388a8801befd543
Simon <simon.diflorio@gmail.com>
2022-10-23 16:30:53 +1000

    sharp api, and api file renaming

---
+++ server/api/sharp-api.js
@@ -0,0 +1,12 @@
+/* CAB432 Assignment 2
+
+    Author: Simon Di Florio
+    Date: 23/10/2022
+
+    References:
+    https://sharp.pixelplumbing.com/
+
+    This file contains API code for SharpJS
+*/
+
+const sharp = require('sharp');
